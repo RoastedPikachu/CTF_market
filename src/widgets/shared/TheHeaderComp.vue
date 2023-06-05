ca6e15c1e573a765b3f6f47732e8ef8725961cc2
Boris Karabut <boriskarabytwork@gmail.com>
2023-06-05 13:58:59 +0300

    Немного поменять удаление cookie

--- src/widgets/shared/TheHeaderComp.vue
+++ src/widgets/shared/TheHeaderComp.vue
@@ -240,7 +240,16 @@
       store.dispatch('clearShoppingCart');
       store.dispatch('clearIsCookieOpen');
 
-      document.cookie = 'NAME=token; path=/; expires=-1';
+      (function deleteAllCookies() {
+          let cookies = document.cookie.split(";");
+
+          for (let i = 0; i < cookies.length; i++) {
+              let cookie = cookies[i];
+              let eqPos = cookie.indexOf("=");
+              let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+              document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+          }
+      })();
 
       window.location.reload();
   }
